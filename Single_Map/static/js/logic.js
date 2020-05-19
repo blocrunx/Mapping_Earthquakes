c570e9c32de29a9d171136f3c2690262265ae75e
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-19 14:25:12 -0400

    initial map

---
+++ Single_Map/static/js/logic.js
@@ -0,0 +1,16 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+console.log(API_KEY)
+
+// Create the map object with a center and zoom level.
+var mymap = L.map('mapid').setView([39.09, -94.57], 4);
+
+// We create the tile layer that will be the background of our map.
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(mymap);
