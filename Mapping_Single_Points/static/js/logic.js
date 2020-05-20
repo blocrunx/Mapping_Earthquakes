a82e4bbce678ae4060e3f52f2796a935d201cc41
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-20 12:29:01 -0400

    dark circle marker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,14 +3,22 @@ console.log("working");
 console.log(API_KEY)
 
 // Create the map object with a center and zoom level.
-var mymap = L.map('mapid').setView([39.09, -94.57], 4);
+var map = L.map('mapid').setView([39.09, -94.57], 4);
 
+// Add a circle marker
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillcolor: "#ffff99"
+ }).addTo(map);
+
+ //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 // We create the tile layer that will be the background of our map.
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
-}).addTo(mymap);
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
