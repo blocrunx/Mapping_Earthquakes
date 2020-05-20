ddec7b615277f5d8c075f08614ef7f2f1eb57265
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-20 14:18:17 -0400

    dark map w/ circle city markers in amber sized by pop

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,3 +1,6 @@
+// Get data from cities.js
+let cityData = cities;
+
 // Add console.log to check to see if our code is working.
 console.log("working");
 console.log(API_KEY)
@@ -5,18 +8,22 @@ console.log(API_KEY)
 // Create the map object with a center and zoom level.
 var map = L.map('mapid').setView([39.09, -94.57], 4);
 
-// Add a circle marker
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillcolor: "#ffff99"
- }).addTo(map);
+  // Loop through the cities array and create one marker for each city.
+  cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000,
+    color: '#ffbf00',
+    fillColor: '#ff8300'})
+    .bindPopup("<h2>" + city.city + ", " +  city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>" ).addTo(map);
+ });
 
  //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
 	accessToken: API_KEY
 });
 
